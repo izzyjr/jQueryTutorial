e6e602f8b759401d8ae768382d2ea224bcda712a
Israel Mesa <mesa.israel50@gmail.com>
2017-09-04 20:41:45 +0000

    vid1

---
+++ jQueryTutorial.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        // $("p").hide(); //HTML element reference
+        // $("#cereal").hide(); //ID reference
+        $(".unicycle").hide(); //HTML class reference
+    });
+});
+
+// jQuery is for simplifying AJAX calls and DOM manipulation
